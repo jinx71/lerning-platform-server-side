1a768443b1ce11bff6ff2e5856be63842a90ca46
jinx71 <karim.sazed@gmail.com>
2022-10-26 00:53:17 +0600

    Server Setup done

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3001;
+// const data = require('./data/categories.json')
+// const news = require('./data/news.json')
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+    res.send("Digital Cloud Server is running")
+})
+
+// app.get("/categories", (req, res) => {
+//   res.send(data)
+// });
+// app.get("/home", (req, res) => {
+//   res.send(news)
+// });
+// app.get("/news/:id", (req, res) => {
+//   // console.log(req.params.id)
+//   const id = req.params.id
+//   const selectedNews = news.filter(n => n._id === id)
+//   res.send(selectedNews)
+// })
+// app.get("/category/:id", (req, res) => {
+//   const id = req.params.id
+
+//   if (id === "08") {
+//     res.send(news);
+//   }
+//   else {
+//     const sortedByCategory = news.filter(n => n.category_id === id)
+//     res.send(sortedByCategory)
+//   }
+// })
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
