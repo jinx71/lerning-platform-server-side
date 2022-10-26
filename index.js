6ebbc2f7d2b8cf3058774b7134dec68f7d3d2d81
jinx71 <karim.sazed@gmail.com>
2022-10-27 00:57:27 +0600

    data set setup

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const express = require("express");
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3001;
-// const data = require('./data/categories.json')
+const data = require('./data/data.json')
 // const news = require('./data/news.json')
 app.use(cors())
 
 
 app.get('/', (req, res) => {
-    res.send("Digital Cloud Server is running")
+    res.send(data)
 })
 
+app.get("/courses/:id", (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    const course = data.find(d => d.CourseID == id)
+    res.send(course)
+    console.log(course)
+})
 // app.get("/categories", (req, res) => {
 //   res.send(data)
 // });
